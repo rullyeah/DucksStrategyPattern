d41c08a2c48b4cf7f89ff2498798edd28cdc2bf0
Raúl Nadal <rullyeah@gmail.com>
2017-12-20 22:30:37 +0100

    Test: all ducks swim: Red

---
+++ spec/ducks_spec.rb
@@ -0,0 +1,20 @@
+describe 'Duck specification' do
+  it 'all ducks can swim' do
+    redheadDuck = RedheadDuck.new
+    mallardDuck = MallardDuck.new
+
+    expect('swimming').to eq(redheadDuck.swim)
+    expect('swimming').to eq(mallardDuck.swim)
+  end
+end
+
+class Duck
+  def swim
+  end
+end
+
+class RedheadDuck < Duck
+end
+
+class MallardDuck < Duck
+end
